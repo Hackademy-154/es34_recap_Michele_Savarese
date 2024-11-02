da073b9bf5b422f46a26a93851d8f7aeac8e482c
DragonautMk <michelesavarese1997@gmail.com>
2024-11-02 12:04:14 +0100

    completato login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,14 +9,30 @@
             </div>
         </div>
 
-<div class="row">
-    <div class="col-12 col-md6">
 
-<form action="
-"></form>
 
-    </div>
-</div>
+
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6">
+
+                <form action="{{ route('login') }}" method="POST" class="rounded shadow p-5 bg-white">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email:</label>
+                        <input type="email" class="form-control" name="email">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password:</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                    <div class="mb-3 text-center pt-3">
+                        <button type="sumbit" class="btn btn-dark">Accedi</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
 
 
     </div>
