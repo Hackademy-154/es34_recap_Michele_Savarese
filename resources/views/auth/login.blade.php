b5969c70295be3caa2e9ba823b5eec8da1853cb9
DragonautMk <michelesavarese1997@gmail.com>
2024-11-02 11:50:06 +0100

    iniziato auth e fixato resource js non inserito correttamente

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,25 @@
+<x-layout>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="display-3 text-center py-5">
+                    Accedi
+                </h1>
+            </div>
+        </div>
+
+<div class="row">
+    <div class="col-12 col-md6">
+
+<form action="
+"></form>
+
+    </div>
+</div>
+
+
+    </div>
+
+
+</x-layout>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Recap michele savarese</title>
-    @vite(['resources/css/app.css', 'resource/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
+<body class="bg-dark-subtle">
 <x-navbar/>
 
 
